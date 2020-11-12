5b749d1247b3cff0354ff7e91996bf3ed4c26e40
IkerGalardi <contacto.ikergalardi@gmail.com>
2020-11-12 13:18:24 +0100

    Updated HAL

--- src/hal/cpu.h
+++ src/hal/cpu.h
@@ -1,13 +1,18 @@
 #ifndef HAL_CPU_H
 #define HAL_CPU_H
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 
-    int cpu_get_thread();
+int cpu_get_thread_id();
 
-    int cpu_get_el();
+int cpu_get_el();
 
-    void cpu_switch_usermode(long pc);
+void cpu_switch_usermode(long pc);
+
+#ifdef __cplusplus
 }
+#endif
 
 #endif // HAL_CPU_H
--- src/hal/exc.h
+++ src/hal/exc.h
@@ -1,30 +1,35 @@
 #ifndef HAL_EXC_H
 #define HAL_EXC_H
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 
-    typedef void(*vector_handler)(long, long);
+typedef void(*vector_handler)(long, long);
 
-    typedef struct {
-        vector_handler syn_handler;
-        vector_handler irq_handler;
-        vector_handler fiq_handler;
-        vector_handler err_handler;
-    } vector_table;
+typedef struct exc_vector_table {
+    vector_handler syn_handler;
+    vector_handler irq_handler;
+    vector_handler fiq_handler;
+    vector_handler err_handler;
+} exc_vector_table;
 
-    void exc_setup_vector(vector_table handlers);
+void exc_setup_vector(exc_vector_table handlers);
 
-    void exc_enable_syn();
-    void exc_disable_syn();
+void exc_enable_syn();
+void exc_disable_syn();
 
-    void exc_enable_irq();
-    void exc_disable_irq();
+void exc_enable_irq();
+void exc_disable_irq();
 
-    void exc_enable_firq();
-    void exc_disable_firq();
+void exc_enable_firq();
+void exc_disable_firq();
 
-    void exc_enable_err();
-    void exc_disable_err();
+void exc_enable_err();
+void exc_disable_err();
+
+#ifdef __cplusplus
 }
+#endif
 
 #endif // HAL_EXC_H
