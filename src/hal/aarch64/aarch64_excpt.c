c655f924ff40a5a8e5af7fbce7ca1e629a2c5f63
IkerGalardi <contacto.ikergalardi@gmail.com>
2020-11-19 12:05:07 +0100

    Revert "Merge pull request #12 from GalardOS/hw-abstraction"
    This reverts commit 7ee5e62192c593620c940a4d3383668606416ede, reversing
    changes made to c76dfd7a7eef94016d7cef74f7feb96f2ea8a0aa.

---
+++ src/hal/aarch64/aarch64_excpt.c
@@ -0,0 +1,62 @@
+/*
+ * This project is provided under the GNU GPL v2 license, more information can 
+ * found on https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Authors:
+ *    - Iker Galardi
+ */
+
+#include "aarch64_excpt.h"
+#include "aarch64_sysregs.h"
+
+static struct {
+    vect_func sync;
+    vect_func irq;
+    vect_func fiq;
+    vect_func err;
+} arch_vector_table;
+
+void arch_setup_vector(vect_func sync, vect_func irq, vect_func fiq, vect_func err) {
+    arch_vector_table.sync = sync;
+    arch_vector_table.irq = irq;
+    arch_vector_table.fiq = fiq;
+    arch_vector_table.err = err;
+
+    arch_setup_vector_regs();
+}
+
+void synchronous_exception(void) {
+    long esr = arch_sysreg_esr();
+    long elr = arch_sysreg_elr();
+    if(arch_vector_table.sync != 0)
+        arch_vector_table.sync(esr, elr);
+}
+
+void irq_exception(void) {
+    long esr = arch_sysreg_esr();
+    long elr = arch_sysreg_elr();
+    if(arch_vector_table.irq != 0)
+        arch_vector_table.irq(esr, elr);
+}
+
+void fiq_exception(void) {
+    long esr = arch_sysreg_esr();
+    long elr = arch_sysreg_elr();
+    if(arch_vector_table.fiq != 0)
+        arch_vector_table.fiq(esr, elr);
+}
+
+void error_exception(void) {
+    long esr = arch_sysreg_esr();
+    long elr = arch_sysreg_elr();
+    if(arch_vector_table.err != 0)
+        arch_vector_table.err(esr, elr);
+}
