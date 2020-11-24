b9da8661df3c88b0d15d4c547e3dc6fbd71d98f4
IkerGalardi <contacto.ikergalardi@gmail.com>
2020-11-24 14:41:34 +0100

    Adding a common.h file

---
+++ src/common.h
@@ -0,0 +1,16 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+typedef unsigned char byte;
+typedef char sbyte;
+
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned int   uint32;
+typedef unsigned long  uint64;
+typedef char           int8;
+typedef short          int16;
+typedef int            int32;
+typedef long           int64;
+
+#endif // COMMON_H
