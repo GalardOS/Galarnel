a89e17a4961d3fcbb6f555f4a103f7fb8eab4294
IkerGalardi <contacto.ikergalardi@gmail.com>
2020-11-30 19:41:45 +0100

    Started using gpio on console

--- src/hal/bcm2711/console.c
+++ src/hal/bcm2711/console.c
@@ -3,10 +3,19 @@
 #include "hal/bcm2711/constants.h"
 
 #include "bcm2835auxuart/bcm2835auxuart.h"
+#include "bcm2711gpio/bcm2711gpio.h"
 
 #define UART_BASE_ADDRESS MMIO_BASE + 0x00215004
+#define GPIO_BASE_ADDRESS 27
 
 void console_initialize() {
+    bcm2711gpio_initialize(GPIO_BASE_ADDRESS);
+
+    // Configure gpio to redirect uart to pins 14 and 15
+    bcm2771gpio_set_pin_mode(14, GPIO_ALT_FUNC0);
+    bcm2771gpio_set_pin_mode(15, GPIO_ALT_FUNC0);
+    /// TODO: setup clocks
+
     bcm2835auxuart_initialize(UART_BASE_ADDRESS);
 }
 
