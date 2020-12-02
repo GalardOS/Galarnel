e481caa47b66059d9c95d2b9b00fb2ea7e9201a8
IkerGalardi <contacto.ikergalardi@gmail.com>
2020-12-02 11:48:19 +0100

    Configuration searches for source files

--- build/config.lua
+++ build/config.lua
@@ -2,4 +2,22 @@
 --      * bcm2711
 CONFIGURATION_PLATFORM = "bcm2711"
 
+-- Include the platform configuration
 lmake_include("build/configs/" .. CONFIGURATION_PLATFORM .. ".lua")
+
+-- Variables to be used in lmake.lua for compiling and linking
+C_SOURCE_FILES = ""
+CC_SOURCE_FILES = ""
+ASM_SOURCE_FILES = ""
+
+-- Add configured driver source files
+for i in 1, #CONFIGURATION_BAKED_DRIVERS do
+    local driver = CONFIGURATION_BAKED_DRIVERS[i]
+    C_SOURCE_FILES = C_SOURCE_FILES .. lmake_find("drivers/" .. driver .. "/**.c")
+    CC_SOURCE_FILES = CC_SOURCE_FILES .. lmake_find("drivers/" .. driver .. "/**.cc")
+end
+
+-- Add ISA source files
+C_SOURCE_FILES = C_SOURCE_FILES .. lmake_find("arch/" .. CONFIGURATION_ISA .. "/**.c")
+CC_SOURCE_FILES = C_SOURCE_FILES .. lmake_find("arch/" .. CONFIGURATION_ISA .. "/**.cc")
+ASM_SOURCE_FILES = C_SOURCE_FILES .. lmake_find("arch/" .. CONFIGURATION_ISA .. "/**.S")
