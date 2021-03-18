bb1cecb317969a2eb7d0cda4760d025c1f732d36
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-03-18 11:19:14 +0100

    Adding some more functions to intc driver

--- src/drivers/bcm2835intc.hh
+++ src/drivers/bcm2835intc.hh
@@ -1,7 +1,12 @@
 #pragma once 
 
+#include "../../lib/libsteel/lib/common.hh"
+
 namespace drv {
     namespace bcm2835intc {
         void initialize();
+
+        void enable_arm_irq(uint32 irq);
+        void enable_peripheral_irq(uint32 irq);
     }
 }
--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,11 @@
 #include "../lib/libsteel/lib/devices/mini_uart.hh"
 
+#include "drivers/bcm2835intc.hh"
+
 void main(int argc, char** argv) {
     steel::uart_send_string("Hello World from iquernel!\r\n");
+
+    drv::bcm2835intc::initialize();
 
     while(true);
 }
